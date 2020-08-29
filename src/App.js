5fdb75653b4a5c2467e22911b22246196e1c4139
dominicdaviescodes <codewithdom@gmail.com>
2020-08-29 20:42:48 +0100

    collect todo input values

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // Import components
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>A React Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
       <TodoList />
     </div>
   );
