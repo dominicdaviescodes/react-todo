c63d485cbf3e19c68012d1f3b8e0b910a7500503
dominicdaviescodes <codewithdom@gmail.com>
2020-08-29 19:54:15 +0100

    creat form component, import to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+// Import components
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>A React Todo List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
